eef86939883304f4822ef3058658c2e622e3fd50
Caroline <gekniateh@gmail.com>
2024-04-26 22:11:56 +0800

    connect to mongodb, create .env

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 //Connecting to mongodb
 require('dotenv').config();
 require('./config/database')
 
 const app = express();
+app.use(express.json());
 
 //Put api use here
 
 //defining port and listen
-const port = 3001;
+const port = 3005;
 
 app.listen(port, function() {
     console.log(`Express app is running on port ${port}`);
