434e502a8ba4f751d5aef3d1b533b1d6823b0663
Caroline <gekniateh@gmail.com>
2024-04-28 14:36:37 +0800

    Create get listing/s, delete listing, update listing functions and routes. Butcannot test it as i cannot create listing w/o being a user first

--- controllers/listing.controllers.js
+++ controllers/listing.controllers.js
@@ -10,6 +10,53 @@ async function createListing(req, res) {
   }
 }
 
+async function getListings(req, res) {
+  try {
+    const listings = await Listing.find({});
+    return res.status(200).json(listings);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+//get only 1 listing
+async function getListing(req, res) {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    return res.status(200).json(listing);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+async function deleteListing(req, res) {
+  try {
+  await Listing.findByIdAndDelete(req.params.id);
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+async function updateListing(req, res) {
+  try {
+  await Listing.findByIdAndUpdate(
+    req.params.id, 
+    req.body,
+    { new: true }
+    );
+    res.status(200).json('updatedListing');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+
+// I can't create new listings via postman tho, think i need to be "logged in" as a user first?
 // To add in deleteListing function, updateListing, getListing
 
-module.exports = { createListing };
+module.exports = { createListing, getListings, getListing, deleteListing, updateListing,  };
--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { createListing } = require("../controllers/listing.controllers"); 
+const listingCtrl = require("../controllers/listing.controllers"); 
 
-router.post("/create", createListing);  
+router.post("/create", listingCtrl.createListing);  
+//http://localhost:3005/api/listing/create
+
+router.get("/get", listingCtrl.getListings); 
+http://localhost:3005/api/listing/all
+
+router.get("/get/:id", listingCtrl.getListing); 
+http://localhost:3005/api/listing/get/id
+
+router.delete('delete/:id', listingCtrl.deleteListing);
+//http://localhost:3005/api/listing/delete/id
+
+router.delete('update/:id', listingCtrl.updateListing);
+//http://localhost:3005/api/listing/update/id
 
 module.exports = router;
