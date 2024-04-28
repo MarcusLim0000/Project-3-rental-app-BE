f0efd5fda4582111a2d1d71f4bc36cbf24a789d8
Caroline <gekniateh@gmail.com>
2024-04-28 17:42:23 +0800

    Changes to listing controllers & routes, tested in postman, del, post, get

--- controllers/listing.controllers.js
+++ controllers/listing.controllers.js
@@ -33,8 +33,13 @@ async function getListing(req, res) {
 
 async function deleteListing(req, res) {
   try {
-  await Listing.findByIdAndDelete(req.params.id);
-    
+    const { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  if (!deletedListing) {
+    return res.status(404).json({ error: 'Listing not found' });
+  }
+console.log(deleteListing, "Listing deleted");
+  res.json({ message: 'Listing deleted successfully' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: error.message });
@@ -43,12 +48,15 @@ async function deleteListing(req, res) {
 
 async function updateListing(req, res) {
   try {
-  await Listing.findByIdAndUpdate(
-    req.params.id, 
+    const { id } = req.params;
+  const updatedListing = await Listing.findByIdAndUpdate(
+    id.trim(), 
     req.body,
     { new: true }
     );
-    res.status(200).json('updatedListing');
+    console.log(updatedListing, "Updated Listing");
+    res.status(200).json(updatedListing);
+    
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: error.message });
--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -6,15 +6,15 @@ router.post("/create", listingCtrl.createListing);
 //http://localhost:3005/api/listing/create
 
 router.get("/get", listingCtrl.getListings); 
-http://localhost:3005/api/listing/all
+http://localhost:3005/api/listing/get
 
 router.get("/get/:id", listingCtrl.getListing); 
 http://localhost:3005/api/listing/get/id
 
-router.delete('delete/:id', listingCtrl.deleteListing);
+router.delete('/delete/:id', listingCtrl.deleteListing);
 //http://localhost:3005/api/listing/delete/id
 
-router.delete('update/:id', listingCtrl.updateListing);
+router.post('/update/:id', listingCtrl.updateListing);
 //http://localhost:3005/api/listing/update/id
 
 module.exports = router;
