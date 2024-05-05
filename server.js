bfd7aa29a6af2d1178c4efa8b212c2b14f16af15
Marcus Lim <marcus.lwc7@gmail.com>
2024-05-05 11:11:18 +0800

    added multer image upload to BE, but only uses local storage and code is not yet refactored into controllers, all in server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors')
+//importing multer uploading below  * FROM MARCUS 5/5
+const multer = require('multer')
 
 //Connecting to mongodb
 require('dotenv').config();
@@ -19,6 +21,23 @@ app.use('/api/users', require('./routes/user.routes'));
 const ensureLoggedIn = require('./config/ensureLoggedIn');
 app.use('/api/listing', ensureLoggedIn, require('./routes/listing.routes'));
 
+//testing uploading function below * FROM MARCUS 5/5
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, './images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), function(req, res){
+    res.json({upload: 'success'})
+});
+
+//test ends here
 
 //defining port and listen
 const port = 3005;
