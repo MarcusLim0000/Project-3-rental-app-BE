a8316b56a5bfbdcdd2e9c42221671f556aedfcbe
Marcus Lim <marcus.lwc7@gmail.com>
2024-05-08 21:11:50 +0800

    cleaning code

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -16,10 +16,8 @@ async function create (req, res) {
 
 async function signIn(req, res) {
   try {
-    // Find the user by their email address
     const user = await User.findOne({ email: req.body.email });
     if (!user) throw new Error();
-    // Check if the password matches
     const match = await bcrypt.compare(req.body.password, user.password);
     if (!match) throw new Error();
     res.json(createJWT(user));
@@ -42,13 +40,9 @@ async function getUserListings(req, res) {
   }
 }
 
-// To add updateUser, deleteUser, getUserListing
-
-/*-- Helper Functions --*/
 
 function createJWT(user) {
   return jwt.sign(
-    // data payload
     { user },
     process.env.SECRET,
     { expiresIn: "24h" }
